371063940263de79111f958a8ae0f3e644898a0f
WS-Galib <washifshadman@gmail.com>
2026-03-17 23:34:10 +0600

    last commit

---
+++ Lab Performance_MID/script.js
@@ -0,0 +1,26 @@
+const unitPrice = 1000;
+const days = 30;
+
+const quantityInput = document.getElementById('quantity');
+const totalPriceInput = document.getElementById('totalPrice');
+
+function calculateTotal() {
+    let quantity = quantityInput.value;
+    if (quantityInput.value == "") {
+        quantity = 0;
+    }
+
+    if (quantity < 0) {
+        quantity = 0;
+        quantityInput.value = 0;
+    }
+
+    let total = unitPrice * quantity * days;
+    totalPriceInput.value = total;
+
+    if (total > 1000) {
+      alert("Congratulations! You are eligible for a gift coupon!");
+    }
+}
+
+quantityInput.addEventListener('input', calculateTotal);
